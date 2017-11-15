ee627cf94e903ca5e0dbe5418725e64220826b97
QUENTIN <qurbani@parsys.com>
2017-11-15 10:32:26 +0100

    ajout pages login, companyinscription, studentinscription

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { StudentinscriptionPage } from '../studentinscription/studentinscription';
+import { CompanyinscriptionPage } from '../companyinscription/companyinscription';
 /**
  * Generated class for the LoginPage page.
  *
@@ -14,12 +16,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'login.html',
 })
 export class LoginPage {
-
+  nav: NavController;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.nav = navCtrl;
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
-
+  public changePage() {
+    this.nav.push(StudentinscriptionPage);
+  }
+  public changePageComp() {
+    this.nav.push(CompanyinscriptionPage);
+  }
 }
+
+
